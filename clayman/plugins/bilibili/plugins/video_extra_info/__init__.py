8201fa52b57d3d82877a79715db46b38d279ae17
hukening <hukening8@gmail.com>
2022-11-20 14:06:38 +0800

    Feature: 显示 bilibili 视频的信息
    使用 bs4 lxml aiohttp

--- clayman/plugins/bilibili/plugins/video_extra_info/__init__.py
+++ clayman/plugins/bilibili/plugins/video_extra_info/__init__.py
@@ -1,15 +1,29 @@
-from nonebot import get_driver
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""显示 Bilibili 视频详细信息的功能
+"""
+
+from nonebot import get_driver, on_message
+from nonebot.rule import regex, to_me
+from nonebot.matcher import Matcher
+from nonebot.params import RegexMatched
 
 from .config import Config
+from . import data_source
 
 global_config = get_driver().config
 config = Config.parse_obj(global_config)
 
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
+VideoInfo = on_message(rule=regex(r"BV\S{10}") & to_me())
 
-# @export.xxx
-# def some_function():
-#     pass
 
+@VideoInfo.handle()
+async def video_info_first_receive(matcher=Matcher, bvid=RegexMatched()):
+    video_info = await data_source.get_video_info_by_bvid(bvid)
+    if video_info:
+        await matcher.finish(
+            "标题：{title}\n作者: {author}\n发布日期: {pubdate}\n简介: {desc}".format(**video_info)
+        )
+    else:
+        await matcher.finish("信息检索失败")
---
+++ clayman/plugins/bilibili/plugins/video_extra_info/data_source.py
@@ -0,0 +1,47 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-~
+
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+def _parse_video_info_from_html(html_str: str):
+    soup = BeautifulSoup(html_str, "lxml")
+    # 标题 作者 简介
+    video_title = soup.find(name="h1", class_="video-title tit").text.strip()
+    video_author = (
+        soup.find(name="div", class_="up-info_right")
+        .find(name="a", class_="username")
+        .text.strip()
+    )
+    video_desc = soup.find(name="span", class_="desc-info-text").text.strip()
+
+    # 发布日期 播放数
+    video_data_list_div = soup.find(name="div", class_="video-data-list")
+    video_pubdate = video_data_list_div.find(
+        name="span", class_="pudate-text"
+    ).text.strip()
+    video_viewcount = video_data_list_div.find(
+        name="span", class_="view item"
+    ).text.strip()
+
+    # TODO(hukening):添加获取封面的支持
+    # video_cover = soup.find(name="div",class_="video-capture").find(name="picture",class_="b-img__inner").src
+
+    return {
+        "title": video_title,
+        "author": video_author,
+        "desc": video_desc,
+        "pubdate": video_pubdate,
+        "viwecount": video_viewcount,
+    }
+
+
+async def get_video_info_by_bvid(bvid: str):
+    async with aiohttp.ClientSession("https://www.bilibili.com") as session:
+        async with session.get(f"/video/{bvid}") as response:
+            if response.status == 200:
+                return _parse_video_info_from_html(await response.text())
+            else:
+                # return None if failed.
+                pass
